d5ea8864f2843fada4b1783e31b25d2eacbdb032
Leaf-Spring <foliaprintempo@gmail.com>
2025-02-26 01:37:21 -0600

    Eliminando target/ del repositorio.

--- target/debug/deps/conociendo-713498563823d3db.d
+++
@@ -1,9 +0,0 @@
-/mnt/Data/Proyectos/Rust/conociendo/target/debug/deps/libconociendo-713498563823d3db.rmeta: src/main.rs Cargo.toml
-
-/mnt/Data/Proyectos/Rust/conociendo/target/debug/deps/conociendo-713498563823d3db.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
