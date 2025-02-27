500d33bd27587a377c9287e5d6232d4eae885f29
Leaf-Spring <foliaprintempo@gmail.com>
2025-02-26 21:21:10 -0600

    Variables.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    println!("{} {} {}", x.0, x.1, x.2);
 }
