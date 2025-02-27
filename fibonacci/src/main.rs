9d2025ec833d935fd8d591fe0fce81ea71ab7304
Leaf-Spring <foliaprintempo@gmail.com>
2025-02-27 01:55:22 -0600

    Fibonacci finish.

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,21 +1,32 @@
+use std::io;
+
 fn main() {
-    for i in 1..=37 {
-        println!("[{i}]: {}", fibonacci(i));
+    loop {
+        println!("Get the Fibonnaci number: ");
+
+        let mut n = String::new();
+
+        io::stdin().read_line(&mut n).expect("Failed to read line.");
+
+        let n: u32 = match n.trim().parse() {
+            Ok(m) => m,
+            Err(_) => break,
+        };
+
+        println!("[{n}]: {}", fibonacci(n));
     }
 }
 
 fn fibonacci(n: u32) -> u32 {
-    if 1 == n || 0 == n {
+    if 0 == n {
         return 0;
-    }
-
-    if 2 == n {
+    } else if 1 == n {
         return 1;
     }
 
     let mut a: u32 = 1;
     let mut b: u32 = 0;
-    for _i in 3..=n {
+    for _ in 2..=n {
         let c = a;
         a += b;
         b = c;
