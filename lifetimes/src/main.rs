608e7bd60afe38d6a407864bf4fcb8ef2709d094
Leaf-Spring <foliaprintempo@gmail.com>
2025-03-03 01:47:39 -0600

    Lifetimes.

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let mut string1 = String::from("long string is long");
+    let string1 = String::from("long string is long");
     let string2 = String::from("xyz");
-    string1 = longest(string1.as_str(), string2.as_str()).to_string();
-    println!("The longest string is {string1}");
+    let r = longest(string1.as_str(), string2.as_str()).to_string();
+    println!("The longest string is {r}");
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
